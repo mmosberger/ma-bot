bc9e4e959a10ec41fad3868b7357cfe029c1ef1a
Knirpsii <47773156+Knirpsii@users.noreply.github.com>
2021-08-10 21:34:05 +0200

    -

--- index.js
+++ index.js
@@ -1,22 +1,51 @@
-const Discord = require('discord.js');
-require("dotenv").config()
-let db = require("./database")
+const { Client, MessageEmbed } = require('discord.js');
+const mysql = require("mariadb");
+require("dotenv").config();
 
-const client = new Discord.Client();
+const client = new Client();
 
-client.on('ready', () => {
-    db.init()
-    console.log(`Logged in as ${client.user.tag}!`);
+let pool = mysql.createPool({
+    "host": "127.0.0.1",
+    "user": process.env.DB_USER,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_DATABASE
 });
 
-client.on('message', message => {
-    if (message.author.bot){
-        return;
-    }
+pool.getConnection().then((conn) => {
+    console.log("DB connected")
+}).catch((err) => {
+    console.error("Unable to connect to the MariaDB database." + err);
+});
 
-    if (message.content === 'ping') {
-        message.reply('pong');
-    }
+// Ready event
+client.on('ready', () => {
+    console.log(`${client.user.tag} is ready!`);
+});
+
+
+client.on('message', async (message) => {
+
+    if (message.author.bot || !message.guild) return;
+
+
+
 });
 
 client.login(process.env.TOKEN);
+
+
+async function query(string, values) {
+    return new Promise((resolve, reject) => {
+        pool.query(string, values, (error, results) => {
+            if (error) {
+                reject(error);
+            } else {
+                resolve(results);
+            }
+        }).then(r =>
+            resolve(r)
+        ).catch(O_o => {
+            console.log(O_o);
+        });
+    });
+}
