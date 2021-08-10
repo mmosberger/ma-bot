01063fa855310c5044f2cb2eb8307c484a3d850f
Knirpsii <47773156+Knirpsii@users.noreply.github.com>
2021-08-10 22:10:56 +0200

    -

--- database.js
+++ database.js
@@ -1,57 +1,36 @@
-/**
- *Description of database.js
- *@author Michel Mosberger
- *@version 1.0
- *@since 10.08.2021
- */
+const mysql = require("mysql");
 
-
-
-const mysql = require("mariadb");
-
-let db = {};
-
-
-async function init() {
-    db.pool = mysql.createPool({
-        "host": "127.0.0.1",
-        "user": process.env.DB_USER,
-        "password": process.env.DB_PASSWORD,
-        "database": process.env.DB_PASSWORD
-    });
-
-    /**
-     * Get db connection or throw error
-     */
-    db.pool.getConnection().then((conn) => {
-        console.log("DB connected")
-    }).catch((err) => {
-        console.error("Unable to connect to the MariaDB database." + err);
-    });
-}
-
-
-// Make a new query to the db
-async function query(string, values) {
-    return new Promise((resolve, reject) => {
-        db.pool.query(string, values, (error, results) => {
-            if (error) {
-                reject(error);
-            } else {
-                resolve(results);
+class Database {
+    static query = async (string, values) => {
+        let pool = mysql.createConnection(
+            {
+                host: "127.0.0.1",
+                user: process.env.USER,
+                password: process.env.PASSWORD,
+                database: process.env.DATABASE
             }
-        }).then(r =>
-            resolve(r)
-        ).catch(O_o => {
-            console.log(O_o);
+        );
+
+        pool.connect(function (err) {
+            if (err) {
+                console.log("Unable to connect to the MySQL database." + err);
+            }
         });
-    });
+
+        return new Promise((resolve, reject) => {
+            pool.query(string, values, (error, results) => {
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve(results);
+                }
+            });
+
+            pool.end();
+
+        });
+    }
 }
 
-db.query = query()
-db.init = init()
 
-module.exports = {
-    query,
-    init
-}
+module.exports = Database;
--- index.js
+++ index.js
@@ -1,21 +1,18 @@
 const { Client, MessageEmbed } = require('discord.js');
-const mysql = require("mariadb");
 require("dotenv").config();
+let db = require("./database")
+
+const symbols = [
+    "↔", "∏", "↕", "―", "Ѱ", "ф", "Σ", "˄", "X", "∟", "≤", "≠", "U", "=", "!", "0"
+]
+const numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
+let pickedSymbolsLegende = [];
+let pickedSymbols = [];
+let answerNumbers = [];
+
 
 const client = new Client();
 
-let pool = mysql.createPool({
-    "host": "127.0.0.1",
-    "user": process.env.DB_USER,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_DATABASE
-});
-
-pool.getConnection().then((conn) => {
-    console.log("DB connected")
-}).catch((err) => {
-    console.error("Unable to connect to the MariaDB database." + err);
-});
 
 // Ready event
 client.on('ready', () => {
@@ -27,25 +24,49 @@ client.on('message', async (message) => {
 
     if (message.author.bot || !message.guild) return;
 
+    let prefix = process.env.PREFIX;
+    let args = message.content.slice(prefix.length).trim().split(/ +/g);
+    let command = args.shift().toLowerCase();
+
+    if (!message.content.startsWith(prefix)) return;
+    if (message.guild && !message.member) await message.guild.fetch.members(message.author);
+
+    let member = message.guild.member(message.mentions.users.first() || args[0]);
+
+    switch(command){
+        case 'create':
 
 
+            break;
+    }
 });
 
 client.login(process.env.TOKEN);
 
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
-async function query(string, values) {
-    return new Promise((resolve, reject) => {
-        pool.query(string, values, (error, results) => {
-            if (error) {
-                reject(error);
-            } else {
-                resolve(results);
-            }
-        }).then(r =>
-            resolve(r)
-        ).catch(O_o => {
-            console.log(O_o);
-        });
-    });
+function create_symbols(){
+    for (let i = 0; i < 9; i++) {
+        let int = getRndInteger(0, (symbols.length - 1));
+
+        while (pickedSymbolsLegende.includes(symbols[int])) {
+            int = getRndInteger(0, (symbols.length) - 1);
+        }
+
+        pickedSymbolsLegende.push(symbols[int]);
+    }
+
+    for (let i = 0; i < 100; i++) {
+
+        let num = getRndInteger(0, 8)
+
+        while (pickedSymbolsLegende[i-1] === pickedSymbolsLegende[num]) {
+            num = getRndInteger(1,9);
+        }
+        answerNumbers.push(num)
+        pickedSymbols.push(pickedSymbolsLegende[num]);
+
+    }
 }
