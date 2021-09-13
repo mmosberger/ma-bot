0bc59986c5585ccb041c8f8fd2e2bb9b46191cc2
Knirpsii <michel.j.mosberger@gmail.com>
2021-09-13 22:13:54 +0200

    made url shorter, possibility for same url now is 62^15

--- functions.js
+++ functions.js
@@ -188,7 +188,7 @@ class statics {
         let result = '';
         let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
         let charactersLength = characters.length;
-        for (let i = 0; i < 30; i++) {
+        for (let i = 0; i < 15; i++) {
             result += characters.charAt(Math.floor(Math.random() * charactersLength));
         }
         return result;
